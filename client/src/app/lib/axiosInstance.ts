0427a2cce4f99480fc4e05ab894273412a6d92ba
Bodhisattwa Basu <basubodhisattwa6@gmail.com>
2025-04-16 22:11:23 +0530

    axios instance updated with backend url andeslint config updated

--- client/src/app/lib/axiosInstance.ts
+++ client/src/app/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000', // Your FastAPI URL
+  baseURL: 'https://rxume.onrender.com', // Your FastAPI URL
 });
 
 export default axiosInstance;
